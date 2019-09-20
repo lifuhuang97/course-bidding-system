5846be291de8313e1a90d0739fbf7d3a684e3edd
tohwangting <54775857+tohwangting@users.noreply.github.com>
2019-09-20 16:21:58 +0800

    Completed Bid.php

---
+++ app/include/Bid.php
@@ -0,0 +1,41 @@
+<?php
+
+class Bid {
+
+    private $userid;
+    private $amount;
+    private $code;
+    private $section;
+    
+    public function __construct($userid, $amount, $code, $section) {
+        $this->userid = $userid;
+        $this->amount = $amount;
+        $this->code = $code;
+        $this->section = $section;
+    }
+
+    public function getUserid() {
+        return $this->userid;
+    }
+
+    public function getAmount() {
+        return $this->amount;
+    }
+
+    public function getCode() {
+        return $this->code;
+    }
+
+    public function getSection() {
+        return $this->section;
+    }
+
+    public function getCourseDetailsByCourseSection(){
+        $courseDAO= new CourseDAO();
+        $course=$courseDAO->RetrieveAllCourseDetail($this->code,$this->section);
+        return $course[0]; // cuz only 1 item
+    }
+
+}
+
+?>
