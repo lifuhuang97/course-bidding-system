f386143fd96c83ccdefc5b23f65d0770999a9645
tohwangting <54775857+tohwangting@users.noreply.github.com>
2019-09-20 16:22:29 +0800

    Completed CourseCompleted.php

---
+++ app/include/CourseCompleted.php
@@ -0,0 +1,24 @@
+<?php
+
+class CourseCompleted {
+
+    private $userid;
+    private $code;
+    
+    public function __construct($userid, $code) {
+        $this->userid = $userid;
+        $this->code = $code;
+    }
+
+    public function getUserid() {
+        return $this->userid;
+    }
+
+    public function getCode() {
+        return $this->code;
+    }
+
+
+}
+
+?>
