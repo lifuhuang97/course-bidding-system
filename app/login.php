5bb8e333e7186c627725859ce6f0d3ba542b1972
Ngo Thanh Van, Christine <tvngo.2018@smu.edu.sg>
2019-09-20 21:39:03 +0800

    Create login.php

---
+++ app/login.php
@@ -0,0 +1,36 @@
+<?php
+    require_once 'include/common.php';
+?>
+
+<html>
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+        <form style="text-align:center; padding:200px;">
+        <h1>Login</h1>
+        <?php
+            // Display error messages
+            if (isset($_SESSION['errors'])){
+                $msg = $_SESSION['errors'];
+                printErrors();
+                unset ($_SESSION['errors']);
+                echo "<p style='color:red'>$msg</p>";
+            }
+                
+        ?>
+        <table align='center'>
+            <tr>
+                <th>UserID</th>
+                <td><input name='username' type='text'></td>
+            </tr>
+            <tr>
+                <th>Password</th>
+                <td><input name='password' type='password'></td>
+            </tr>
+        </table>
+        <br>
+        <input type='submit' value='Sign In'>
+        </form>
+    </body>
+</html>
