8b234bd514756910fea853642316ce5ad17f4789
tohwangting <54775857+tohwangting@users.noreply.github.com>
2019-09-20 16:23:45 +0800

    Completed Student.php

---
+++ app/include/Student.php
@@ -0,0 +1,42 @@
+<?php
+
+class Student {
+
+    private $userid;
+    private $password;
+    private $name;
+    private $school;
+    private $edollar;
+    
+    public function __construct($userid, $password, $name, $school, $edollar) {
+        $this->userid = $userid;
+        $this->password = $password;
+        $this->name = $name;
+        $this->school = $school;
+        $this->edollar = $edollar;
+    }
+    
+    public function getUserid() {
+        return $this->userid;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getSchool() {
+        return $this->school;
+    }
+
+    public function getEdollar() {
+        return $this->edollar;
+    }
+
+
+}
+
+?>
