20478023ee25536ef05fa81f9cbde351efd145ec
TayYuLiang <54829866+TayYuLiang@users.noreply.github.com>
2019-09-22 20:45:07 +0800

    Update logout.php
    Co-Authored-By: teeyong <teeyong@users.noreply.github.com>

--- app/logout.php
+++ app/logout.php
@@ -1,7 +1,7 @@
 <?php
     require_once 'include/common.php';
     session_unset();
-    session_destrol();
+    session_destroy();
     echo"<strong>You have successfully logged out of the system. Please close this browser window.</strong>";
     echo "<a href='login.php'>Click here to go back</a>";
 ?>
