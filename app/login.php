315c566bb54c2efbd03197e1750960ebe5f020fb
Ngo Thanh Van, Christine <tvngo.2018@smu.edu.sg>
2019-09-26 20:50:45 +0800

    Created login.css; updated UI of login screen
    Co-Authored-By: tayyuliang <tayyuliang@users.noreply.github.com>

--- app/login.php
+++ app/login.php
@@ -5,31 +5,45 @@
 <html>
     <head>
         <title>Login</title>
+        <link rel="stylesheet" type="text/css" href="login.css">
     </head>
     <body>
-        <form action="processLogin.php" method="POST" style="text-align:center; padding:200px;">
-            <h1>Login</h1>
-            <?php
-                // Display error messages
-                if (isset($_SESSION['errors'])){
-                    $msg = $_SESSION['errors'];
-                    printErrors();
-                    unset ($_SESSION['errors']);
-                }
-                    
-            ?>
-            <table align='center'>
-                <tr>
-                    <th>UserID</th>
-                    <td><input name='username' type='text'></td>
-                </tr>
-                <tr>
-                    <th>Password</th>
-                    <td><input name='password' type='password'></td>
-                </tr>
-            </table>
-            <br>
-            <input type='submit' value='Sign In'>
+        <header>
+            <div class="header-logo">
+                <img src="smulogo-white.png">
+            </div>
+        </header>
+        <div class="boss-title">
+            <img src="bios.png" style="width: 6em">
+            <img src="boss-full.png" class="boss-full">
+        </div>
+        <div class="signin-title">
+            <img src="Headline.png" style="width: 7em">
+            <!-- <img src="line1.png"> -->
+        </div>
+        <div class="login-form">
+            <form action="processLogin.php" method="POST" style="text-align:center">
+                <?php
+                    // Display error messages
+                    if (isset($_SESSION['errors'])){
+                        $msg = $_SESSION['errors'];
+                        printErrors();
+                        unset ($_SESSION['errors']);
+                    }
+                        
+                ?>
+                <table align='center'>
+                    <tr>
+                        <td><input class="username" name='username' type='text' placeholder="UserID"></td>
+                    </tr>
+                    <tr></tr>
+                    <tr>
+                        <td><input class="password" name='password' type='password' placeholder="Password"></td>
+                    </tr>
+                </table>
+                <br>
+                <input class="submit" type='submit' value='Sign In'>
         </form>
+        </div>
     </body>
 </html>
