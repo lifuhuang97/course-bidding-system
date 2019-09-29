9f4d6f786f3d23d5ff9f6bf801330d3f48433e57
teeyong <54571821+teeyong@users.noreply.github.com>
2019-09-29 18:13:54 +0800

    Update common.php with validation function
    Co-Authored-By: tayyuliang <tayyuliang@users.noreply.github.com>

--- app/include/common.php
+++ app/include/common.php
@@ -21,4 +21,42 @@ function printErrors() {
     }    
 }
 
+function isMissingOrEmpty($name) {
+    if (!isset($_REQUEST[$name])) {
+        return "$name cannot be empty";
+    }
+
+    // client did send the value over
+    $value = $_REQUEST[$name];
+    if (empty($value)) {
+        return "$name cannot be empty";
+    }
+}
+
+# check if an int input is an int and non-negative
+function isNonNegativeInt($var) {
+    if (is_numeric($var) && $var >= 0 && $var == round($var))
+        return TRUE;
+}
+
+# check if a float input is is numeric and non-negative
+function isNonNegativeFloat($var) {
+    if (is_numeric($var) && $var >= 0)
+        return TRUE;
+}
+
+# this is better than empty when use with array, empty($var) returns FALSE even when
+# $var has only empty cells
+function isEmpty($var) {
+    if (isset($var) && is_array($var))
+        foreach ($var as $key => $value) {
+            if (empty($value)) {
+               unset($var[$key]);
+            }
+        }
+
+    if (empty($var))
+        return TRUE;
+}
+
 ?>
