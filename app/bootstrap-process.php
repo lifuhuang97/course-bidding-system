6fefdc158707b5d1a6ae2cd854c4ac4f26ba7e2f
teeyong <54571821+teeyong@users.noreply.github.com>
2019-10-16 23:10:26 +0800

    bootstrap restructuring
    Co-Authored-By: tohwangting <tohwangting@users.noreply.github.com>

--- app/bootstrap-process.php
+++ app/bootstrap-process.php
@@ -2,5 +2,16 @@
 # edit the file included below. the bootstrap logic is there
 require_once 'include/bootstrap.php';
 
-doBootstrap();
+$output=doBootstrap();
+// var_dump($output);
+if (isset($output)){
+    echo $output['status'];
+    var_dump ($output['num-record-loaded']);
+    if (isset($output['error'])){
+        foreach($output['error'] as $error){
+            var_dump($error);
+        }
+    }
+}
+exit;
 header('Location: adminMainPage.php');
