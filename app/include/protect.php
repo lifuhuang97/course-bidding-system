108a97b9327eefeadd38f60f9e0ec829bfcad529
teeyong <54571821+teeyong@users.noreply.github.com>
2019-10-18 13:18:09 +0800

    json message from protect.php
    Co-Authored-By: tohwangting <tohwangting@users.noreply.github.com>

--- app/include/protect.php
+++ app/include/protect.php
@@ -2,22 +2,37 @@
 require_once 'token.php';
 require_once 'common.php';
 
-$errors=[];
 $token = '';
-if  (isset($_REQUEST['token'])) {
-    $token = $_REQUEST['token'];
-    $result=verify_token($token);
-    if ($result=="Expired"){ //session expired
-        $_SESSION['errors'] =  ["Session Expired. Please Login Again!"];
-        header('Location:login.php');
-    }elseif($result!=$_SESSION['success']){// wrong username that login
-        $_SESSION['errors'] =  ["Token is invalid. Please Login!"];
-        header('Location:login.php');
+$tokenError='';
+$pathSegments = explode('/',$_SERVER['PHP_SELF']); # Current url
+$numSegment = count($pathSegments);
+$currentFolder = $pathSegments[$numSegment - 2]; # Current folder
+$page = $pathSegments[$numSegment -1]; # Current page
+
+if ($currentFolder == "json")
+{
+    $tokenError = [ isMissingOrEmpty ('token')];
+    $tokenError = array_filter($tokenError);
+    if (isEmpty($tokenError)) {
+        $token = $_REQUEST['token'];
+        if (!verify_token($token)){
+            $tokenError=["invalid token"];
+        }
     }
 }else{
-    $_SESSION['errors'] =  ["Missing token. Please Login!"];
-    header('Location:login.php');
+    if (isset($_REQUEST['token'])) {
+        $token = $_REQUEST['token'];
+        $result=verify_token($token);
+        if ($result=="Expired"){ //session expired
+            $_SESSION['errors'] =  ["Session Expired. Please Login Again!"];
+            header('Location:login.php');
+        }elseif($result!=$_SESSION['success']){// wrong username that login
+            $_SESSION['errors'] =  ["Token is invalid. Please Login!"];
+            header('Location:login.php');
+        }
+    }else{
+        $_SESSION['errors'] =  ["Missing token. Please Login!"];
+        header('Location:login.php');
+    }
 }
-
-
 ?>
