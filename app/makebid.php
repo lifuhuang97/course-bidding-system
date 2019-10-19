e55a3e1a011aa81882a9e7f8ea4648ee9e55ab7b
Ngo Thanh Van, Christine <tvngo.2018@smu.edu.sg>
2019-10-19 23:41:05 +0800

    Update UI for makebid

--- app/makebid.php
+++ app/makebid.php
@@ -51,111 +51,119 @@
 ?>
 <html>
 <head>
+    <title>Add Bid</title>
+    <link rel="stylesheet" type="text/css" href="css/mainpageUI.css">
+    <script src="https://kit.fontawesome.com/129e7cf8b7.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    Welcome, <?=$name?><br>
-    School: <?=$school?><br>
-     Credit Left: <?=$edollar?>
-     <hr>
-     Please place your bid here:
-    <form action="processBid.php?token=<?php echo $_GET['token']?>" method="POST">
-        <input type='hidden' name='eCredit' value="<?=$edollar?>">
-        <table>
-            <tr>
-                <th>Course Code:</th>
-                <td><input type='text' name='code'></td>
-            </tr>
-            <tr>
-                <th>Section ID:</th>
-                <td><input type='text' name='sectionID'></td>
-            </tr>
-            <tr>
-                <th>Bid Amount:</th>
-                <td><input type='text' name='bidAmt'></td>
-            </tr>
-        </table>
-        <input type='submit'>
-        
-    </form>
-<?php
-    if (isset($_SESSION['errors1'])) {
-        foreach ($_SESSION['errors1'] as $errors){
-            print $errors;
-            print "<br>";
-        }
-        unset ($_SESSION['errors1']);
-    }
-    #if (isset($_SESSION['errors2'])) {
-    #    foreach ($_SESSION['errors2'] as $errors){
-    #        print $errors;
-    #        print "<br>";
-    #    }
-    #    unset ($_SESSION['errors2']);
-    #}
-    #if (isset($_SESSION['errors3'])) {
-    #    foreach ($_SESSION['errors3'] as $errors){
-    #        print $errors;
-    #        print "<br>";
-    #    }
-    #    unset ($_SESSION['errors3']);
-    #}
-?>
-<hr>
-Available Course to Bid
-<?php
-if ($roundID==1){
-    if (count($courses)==0){
-        echo "No available course";
-    }else {
-        echo"<table border='1px'>
-        <tr>
-            <th>Course ID</th>
-            <th>Title</th>
-            <th>Section ID</th>
-            <th>Day</th>
-            <th>Lesson Start Time</th>
-            <th>Lesson End Time</th>
-            <th>Instructor</th>
-            <th>Size</th> 
-            <th>Exam Date</th>
-            <th>Exam Start Time</th>
-            <th>Exam End Time</th>
-        </tr>";
+    <div class="container">
+        <div class="navbar-left">
+            <div class="navbar-left__profile">
+                <div class="navbar-left__profile__container">
+                    <div class="profile-picture">
+                        <img class="profpic" src="css/profpic1.png">
+                    </div>
+                    <div class="profile-details">
+                        <p>Welcome, <?=$name?></p>
+                        <p><?=$school?></p>
+                        <p>Credit Balance: <?=$edollar?></p>
+                    </div>
+                </div>
+            </div>
+            <div class="navbar-left__completed">COMPLETED <i class="far fa-window-restore"></i></div>
+            <a href='makebid.php?token=<?php echo $_GET['token']?>' style="color: white; text-decoration: none;"><div class="navbar-left__addCourse">ADD BID <i class="far fa-calendar-plus"></i></div></a>
+            <a href='editBid.php?token=<?php echo $_GET['token']?>' style="color: white; text-decoration: none;"><div class="navbar-left__editBid">EDIT BID <i class="fas fa-pen-square"></i></div></a>
+            <a href='deletebid.php?token=<?php echo $_GET['token']?>' style="color: white; text-decoration: none;"><div class="navbar-left__dropCourse">DROP BID <i class="far fa-calendar-times"></i></div></a>
+            <a href="logout.php" style="color: white; text-decoration: none;"><div class="navbar-left__logout">LOGOUT <i class="fas fa-sign-out-alt"></i></div></a>
+            <div class="navbar-left__smuLogo">
+                    <img src="css/smulogo.png">
+            </div>
+        </div>
+        <div class="display-right">
+            <div class="form-container">
+                <div class="form-header">
+                    <p>Make a bid</p>
+                </div>
+                <form action="processBid.php?token=<?php echo $_GET['token']?>" method="POST">
+                    <input type='hidden' name='eCredit' value="<?=$edollar?>">
+                    <div class="form-group">
+                        <label for="code">Course Code: </label><br>
+                        <input class="form-control" type="text" name="code" required>
+                    </div>
+                    <div class="form-group">
+                       <label for="sectionID">Section ID: </label><br>
+                        <input class="form-control" type="text" name="sectionID" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="bidAmt">Bid Amount: </label><br>
+                        <input class="form-control" type="number" name="bidAmt" required>
+                    </div>
+                    <input class="submit-btn" type='submit' name="submit">
+                </form>
+            </div>
+            <?php
+                if (isset($_SESSION['errors1'])) {
+                    foreach ($_SESSION['errors1'] as $errors){
+                        print $errors;
+                        print "<br>";
+                    }
+                    unset ($_SESSION['errors1']);
+                }
+            ?>
+            <?php
+            if ($roundID==1){
+                if (count($courses)==0){
+                    echo "No available course";
+                }else {
+                    echo"<table border='1px'>
+                    <tr>
+                        <th>Course ID</th>
+                        <th>Title</th>
+                        <th>Section ID</th>
+                        <th>Day</th>
+                        <th>Lesson Start Time</th>
+                        <th>Lesson End Time</th>
+                        <th>Instructor</th>
+                        <th>Size</th> 
+                        <th>Exam Date</th>
+                        <th>Exam Start Time</th>
+                        <th>Exam End Time</th>
+                    </tr>";
 
-        $currentavailable = [];
-        foreach ($courses as $course){
-            // need remove modules that user alr completed and remove modules that the use alr bidded and taking out those courses that require PREREQUISITES (but the user haven't take)
-            if ( !(in_array ($course->getCourseid(), $realarray)) and !(in_array($course->getCourseid(),$biddedmodsarray)) and CheckForCompletedPrerequisites($userid,$course->getCourseid()) ){
-                //print out every mods that the user haven't take and those modules that the user haven't bidded and those courses that require PREREQUISTIES that the user is available
-                echo"<tr>
-                <td>{$course->getCourseid()}</td>
-                <td>{$course->getTitle()}</td>
-                <td>{$course->getSectionid()}</td>
-                <td>{$course->getDay()}</td>
-                <td>{$course->getStart()}</td>
-                <td>{$course->getEnd()}</td>
-                <td>{$course->getInstructor()}</td>
-                <td>{$course->getSize()}</td>
-                <td>{$course->getExamDate()}</td>
-                <td>{$course->getExamStart()}</td>
-                <td>{$course->getExamEnd()}</td>
-                </td>";
-                //storing the available courses 
-                array_push($currentavailable, [$course->getCourseid() , $course->getTitle() , $course->getSectionid() , $course->getDay(), 
-                $course->getStart(), $course->getEnd() , $course->getInstructor() , $course->getSize() , $course->getExamDate() , 
-                $course->getExamStart() , $course->getExamEnd()] );
+                    $currentavailable = [];
+                    foreach ($courses as $course){
+                        // need remove modules that user alr completed and remove modules that the use alr bidded and taking out those courses that require PREREQUISITES (but the user haven't take)
+                        if ( !(in_array ($course->getCourseid(), $realarray)) and !(in_array($course->getCourseid(),$biddedmodsarray)) and CheckForCompletedPrerequisites($userid,$course->getCourseid()) ){
+                            //print out every mods that the user haven't take and those modules that the user haven't bidded and those courses that require PREREQUISTIES that the user is available
+                            echo"<tr>
+                            <td>{$course->getCourseid()}</td>
+                            <td>{$course->getTitle()}</td>
+                            <td>{$course->getSectionid()}</td>
+                            <td>{$course->getDay()}</td>
+                            <td>{$course->getStart()}</td>
+                            <td>{$course->getEnd()}</td>
+                            <td>{$course->getInstructor()}</td>
+                            <td>{$course->getSize()}</td>
+                            <td>{$course->getExamDate()}</td>
+                            <td>{$course->getExamStart()}</td>
+                            <td>{$course->getExamEnd()}</td>
+                            </td>";
+                            //storing the available courses 
+                            array_push($currentavailable, [$course->getCourseid() , $course->getTitle() , $course->getSectionid() , $course->getDay(), 
+                            $course->getStart(), $course->getEnd() , $course->getInstructor() , $course->getSize() , $course->getExamDate() , 
+                            $course->getExamStart() , $course->getExamEnd()] );
+                        }
+                    }
+                    echo"</table>";
+                    $_SESSION['availablecourses'] = $currentavailable;
+                }
+            }elseif ($roundID==2){
+                print('Incomplete, should show all courses that is available for the user,should include the validation stuffs as well ');
+                //should show all courses that is available for the user 
+                //should include the validation stuffs as well
             }
-        }
-        echo"</table>";
-        $_SESSION['availablecourses'] = $currentavailable;
-    }
-}elseif ($roundID==2){
-    print('Incomplete, should show all courses that is available for the user,should include the validation stuffs as well ');
-    //should show all courses that is available for the user 
-    //should include the validation stuffs as well
-}
-?>
-
+            ?>
+        </div>
+    </div>    
 </body>
 </html>
-<a href="mainpage.php?token=<?php echo $_GET['token']?>">Back</a>
